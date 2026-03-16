f4996ae8a73531d7de9b3d37a0b458ed04b3b6b1
Steven Remus <stevenremus@gmail.com>
2026-03-16 16:20:25 -0700

    Fix R2 public media env var name

--- RemusRodeoBred/settings.py
+++ RemusRodeoBred/settings.py
@@ -105,7 +105,7 @@ R2_ACCESS_KEY_ID = os.environ.get("R2_ACCESS_KEY_ID")
 R2_SECRET_ACCESS_KEY = os.environ.get("R2_SECRET_ACCESS_KEY")
 R2_BUCKET_NAME = os.environ.get("R2_BUCKET_NAME")
 R2_ACCOUNT_ID = os.environ.get("R2_ACCOUNT_ID")
-R2_PUBLIC_BASE_URL = os.environ.get("R2_PUBLIC_BASE_URL")
+R2_PUBLIC_BASE_URL = os.environ.get("R2_PUBLIC_MEDIA_URL")
 
 USE_R2 = all(
     [
